e2b3d1f9e70ae92e340e6dab178cdc133c379e78
DCGALLERY <62862057+DCGALLERY@users.noreply.github.com>
2020-05-09 13:47:43 +0300

    update login logout signin

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,38 @@
+db.collection('guides').get().then(snapshot => {
+  setupGuides(snapshot.docs);
+});
+
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+  if (user) {
+    console.log('user logged in: ', user);
+    db.collection('guides').onSnapshot(snapshot => {
+      setupGuides(snapshot.docs);
+      setupUI(user);
+    }, err => console.log(err.message));
+  } else {
+    console.log('user logged out');
+    setupUI();
+    setupGuides([]);
+  }
+})
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('guides').add({
+    title: createForm.title.value,
+    content: createForm.content.value
+  }).then(() => {
+    // close the create modal & reset form
+    $('#modal-create').modal('hide');
+    createForm.reset();
+  }).catch(err => {
+    console.log(err.message);
+  });
+});
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -9,12 +44,45 @@ signupForm.addEventListener('submit', (e) => {
 
   // sign up the user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    return db.collection('users').doc(cred.user.uid).set({
+      name: signupForm['signup-name'].value
+    });
     console.log(cred.user);
+  }).then(() => {
     // close the signup modal & reset form
     //const modal = document.querySelector('#modal-signup');
     //M.Modal.getInstance(modal).close();
-    $('#modalSign').modal('hide');
+    $('#modalSignUp').modal('hide');
     $('#modalVerify').modal('show');
     signupForm.reset();
   });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  })
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    $('#modalSignIn').modal('hide');
+    $('#modalUser').modal('show');
+       loginForm.reset();
+  });
+
 });
